7b6b549f221d968d48bb4f10c7c303e47be254b3
Mario Granados <mario.alberto.granados1@gmail.com>
2023-01-09 15:20:53 -0600

    pokemon api

--- src/Components/PokemonGenerator.jsx
+++ src/Components/PokemonGenerator.jsx
@@ -1,16 +1,52 @@
 import { POKEMON_API } from "../Services/Config";
-import axios from 'axios'
+import axios from "axios";
+import { useState } from "react";
 
 const PokemonGenerator = () => {
-    const handleClick = (e) => {
-        e.preventDefault();
-        axios.get(POKEMON_API + 'pokemon/1' ).then(res => {console.log(res.data)})
+  const [pokemon, setPokemon] = useState(null);
 
-    }
-    return(
-        <>
-        <button className="btn btn-primary" onClick={handleClick}>Fetch</button>
-        </>
-    )
-}
+  const handleClick = (e) => {
+    let randomNumber = (Math.random() * 100).toFixed();
+    console.log(randomNumber);
+    e.preventDefault();
+    axios
+      .get(POKEMON_API + randomNumber.toString())
+      .then((res) => setPokemon({ appInfo: res.data }))
+      .then(console.log(pokemon))
+  };
+
+  const handleRun = (e) => {
+    e.preventDefault();
+    setPokemon(null);
+    handleClick(e);
+  };
+
+  const handleCatch = (e) => {
+    e.preventDefault();
+    //add pokemon id to database
+  }
+  return (
+    <>
+      {pokemon === null ? (
+        <button className="btn btn-primary" onClick={handleClick}>
+          Fetch
+        </button>
+      ) : (
+        <div class="card" style={{ width: "18rem" }}>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.appInfo.id}.png`}
+            class="card-img-top"
+            alt={"sprite image of " + pokemon.appInfo.name}
+          />
+          <div class="card-body">
+            <h5 class="card-title">{pokemon.appInfo.name}</h5>
+            <p class="card-text">{pokemon.appInfo.description}</p>
+            <button className="btn btn-danger" onClick={handleRun}>Run</button>
+            <button className="btn btn-secondary" onClick={handleCatch}>Catch</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 export default PokemonGenerator;
--- src/Services/Config.js
+++ src/Services/Config.js
@@ -3,4 +3,4 @@ export const USERS = URL + 'api/users';
 export const POKEMON = URL + 'api/pokemons'
 export const HEADERS = new Headers();
 
-export const POKEMON_API = "https://pokeapi.co/api/v2/";
+export const POKEMON_API = "https://pokeapi.co/api/v2/pokemon-species/";
