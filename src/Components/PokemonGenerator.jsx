1bd34ee5ff84c8b90a28f75f80ce7b35dbdb88de
Mario Granados <mario.alberto.granados1@gmail.com>
2023-01-18 23:17:58 -0600

    pokemon gen

--- src/Components/PokemonGenerator.jsx
+++ src/Components/PokemonGenerator.jsx
@@ -1,56 +1,57 @@
 import { POKEMON_API } from "../Services/Config";
 import axios from "axios";
 import { catchPokemon } from "../Services/PokemonServices";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const PokemonGenerator = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
   const [pokemon, setPokemon] = useState({
     pokemon_name: "",
     pokemon_number: "",
     favorite: false,
     user_id: 1,
   });
+  const [click, setClick] = useState(false);
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    let randomNumber = (Math.random() * 100).toFixed();
-    axios
-      .get(POKEMON_API + randomNumber.toString())
-      .then((res) => {
-        setData([res.data])
-      })
-      .then(console.log(data));
+  async function apiFetchRequest() {
+    let randomNumber = Math.floor(Math.random() * 100);
+    const results = await axios.get(POKEMON_API + randomNumber.toString());
+    const json = results.data;
+    setData({ api: json });
+    setPokemon({
+      pokedexNumber: json.id,
+      pokemonName: json.name,
+      user: 1,
+    });
+  }
+
+  const handleClick = () => {
+    apiFetchRequest()
+
   };
 
   const handleCatch = (e) => {
     e.preventDefault();
     //add pokemon id to database
-    setPokemon({
-      pokedex_number: data.appInfo.id,
-      pokemon_name: data.appInfo.name,
-      user_id: 1,
-      favorite: false,
-    });
     console.log(pokemon);
-    // catchPokemon(pokeObj);
+    catchPokemon(pokemon);
   };
   return (
     <>
-      {data.length === 0 ? (
+      {data === null ? (
         <button className="btn btn-primary" onClick={handleClick}>
           Fetch
         </button>
       ) : (
-        <div class="card" style={{ width: "18rem" }}>
+        <div className="card" style={{ width: "18rem" }}>
           <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.appInfo.id}.png`}
-            class="card-img-top"
-            alt={"sprite image of " + data.appInfo.name}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.api.id}.png`}
+            className="card-img-top"
+            alt={"sprite image of " + data.api.name}
           />
-          <div class="card-body">
-            <h5 class="card-title">{data.appInfo.name}</h5>
-            <p class="card-text">{data.appInfo.description}</p>
+          <div className="card-body">
+            <h5 className="card-title">{data.api.name}</h5>
+            <p className="card-text">{data.api.description}</p>
             <button className="btn btn-danger" onClick={handleClick}>
               Run
             </button>
